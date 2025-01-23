a59862289dd8556f6e5681a45b907c259f7d5457
sourav das <sourav99166@gmail.com>
2025-01-23 16:13:20 +0530

    Code push

---
+++ Submodule/Submodule/Twosum.cs
@@ -0,0 +1,21 @@
+﻿namespace Submodule
+{
+    public class Twosum
+    {
+        public static int[] FindTwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = [];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (map.TryGetValue(complement, out int value))
+                {
+                    return [value, i];
+                }
+                map[nums[i]] = i;
+            }
+            throw new Exception("No two sum solution");
+        }
+    }
+
+}
